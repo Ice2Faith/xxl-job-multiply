093638f13b8750c1101303d55b3ad7d918207947
Ice2Faith <Ugex_Savelar@163.com>
2024-05-25 16:43:05 +0800

    feat(api): support use api invoke by http header use "token"

--- xxl-job-admin/src/main/java/com/xxl/job/admin/service/LoginService.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/service/LoginService.java
@@ -17,7 +17,7 @@ import org.springframework.util.StringUtils;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author xuxueli 2019-05-04 22:13:264
@@ -26,6 +26,9 @@ import java.io.*;
 public class LoginService {
 
     public static final String LOGIN_IDENTITY_KEY = "XXL_JOB_LOGIN_IDENTITY";
+
+    public static final String LOGIN_API_TOKEN_HEADER ="token";
+    public static final int LOGIN_API_TOKEN_EXPIRE_MINUTES=15;
 
     public static final String KEY_PAIR_PATH=SecurityContext.STORE_PATH+"/token.pk";
 
@@ -48,11 +51,33 @@ public class LoginService {
     private XxlJobUserDao xxlJobUserDao;
 
 
-    private String makeToken(XxlJobUser xxlJobUser){
+    private String[] makeToken(XxlJobUser xxlJobUser){
         xxlJobUser.setPassword(null);
         String tokenJson = JacksonUtil.writeValueAsString(xxlJobUser);
+        long ets=System.currentTimeMillis()+ TimeUnit.MINUTES.toMillis(LOGIN_API_TOKEN_EXPIRE_MINUTES);
+        String apiToken=Long.toHexString(ets)+"#"+tokenJson;
         String tokenHex = Sm2.doEncrypt(tokenJson, keypair.getPublicKey());
-        return tokenHex;
+        String apiTokenHex="expire"+ets+"."+Sm2.doEncrypt(apiToken,keypair.getPublicKey());
+        return new String[]{tokenHex,apiTokenHex};
+    }
+
+    private XxlJobUser parseApiToken(String apiTokenHex){
+        XxlJobUser xxlJobUser = null;
+        if (apiTokenHex != null) {
+            int idx=apiTokenHex.indexOf(".");
+            if(idx>=0){
+                apiTokenHex=apiTokenHex.substring(idx+1);
+            }
+            String tokenJson = cacheParseToken.get(apiTokenHex);
+            String[] arr = tokenJson.split("#", 2);
+            long ts = Long.parseLong(arr[0],16);
+            if(ts<System.currentTimeMillis()){
+                return null;
+            }
+            tokenJson=arr[1];
+            xxlJobUser = JacksonUtil.readValue(tokenJson, XxlJobUser.class);
+        }
+        return xxlJobUser;
     }
 
 
@@ -83,10 +108,13 @@ public class LoginService {
             return new ReturnT<String>(500, I18nUtil.getString("login_param_unvalid"));
         }
 
-        String loginToken = makeToken(xxlJobUser);
+        String[] tokens = makeToken(xxlJobUser);
+        String loginToken=tokens[0];
+        String apiToken=tokens[1];
 
         // do login
         CookieUtil.set(response, LOGIN_IDENTITY_KEY, loginToken, ifRemember);
+        response.setHeader(LOGIN_API_TOKEN_HEADER,apiToken);
         return ReturnT.SUCCESS;
     }
 
@@ -108,11 +136,20 @@ public class LoginService {
      * @return
      */
     public XxlJobUser ifLogin(HttpServletRequest request, HttpServletResponse response){
-        String cookieToken = CookieUtil.getValue(request, LOGIN_IDENTITY_KEY);
-        if (cookieToken != null) {
+        String token = CookieUtil.getValue(request, LOGIN_IDENTITY_KEY);
+        boolean isApiType=false;
+        if(!StringUtils.hasText(token)){
+            token = request.getHeader(LOGIN_API_TOKEN_HEADER);
+            isApiType=true;
+        }
+        if (StringUtils.hasText(token)) {
             XxlJobUser cookieUser = null;
             try {
-                cookieUser = parseToken(cookieToken);
+                if(isApiType){
+                    cookieUser = parseApiToken(token.trim());
+                }else{
+                    cookieUser = parseToken(token.trim());
+                }
             } catch (Exception e) {
                 logout(request, response);
             }
